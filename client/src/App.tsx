ff8454661ecb739ee8c42634d52967e996709e03
rosellerenrqz <enriquezroseller23@gmail.com>
2024-06-20 20:00:43 +0800

    updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+import Signin from "./pages/admin/components/Signin";
 import Main from "./pages/client/Main";
+import AdminDashboard from "./pages/admin/components/AdminDashboard";
 
 const App = () => {
   return (
-    <div>
-      <Main />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/admin" element={<Signin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </>
   );
 };
 
