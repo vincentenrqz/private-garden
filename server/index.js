3455517bab5e1a5c3db134191f1a99c704a1196c
Roseller <enriquezroseller23@gmail.com>
2024-08-06 10:17:30 +0800

    update api endpoint

--- server/index.js
+++ server/index.js
@@ -23,32 +23,32 @@ app.use(cors());
 connectMongoDB();
 
 //User Routes
-app.post("/", UsersController.createUsersData);
-app.get("/", UsersController.getAllUsersData);
-app.get("/:id", UsersController.getUsersDataById);
-app.put("/:id", UsersController.updateUsersData);
-app.delete("/:id", UsersController.deleteUsersData);
+app.post("/user", UsersController.createUsersData);
+app.get("/user", UsersController.getAllUsersData);
+app.get("/user/:id", UsersController.getUsersDataById);
+app.put("/user/:id", UsersController.updateUsersData);
+app.delete("/user/:id", UsersController.deleteUsersData);
 
 //Types Routes
-app.post("/", TypesController.createTypesData);
-app.get("/", TypesController.getAllTypesData);
-app.get("/:od", TypesController.getTypesDataById);
-app.put("/:id", TypesController.updateTypesData);
-app.delete("/:id", TypesController.deleteTypesData);
+app.post("/types", TypesController.createTypesData);
+app.get("/types", TypesController.getAllTypesData);
+app.get("/types/:id", TypesController.getTypesDataById);
+app.put("/types/:id", TypesController.updateTypesData);
+app.delete("/types/:id", TypesController.deleteTypesData);
 
 //Species Routes
-app.post("/", SpeciesController.createSpeciesData);
-app.get("/", SpeciesController.getAllSpeciesData);
-app.get("/:id", SpeciesController.getSpeciesDataById);
-app.put("/:id", SpeciesController.updateSpeciesData);
-app.delete("/:id", SpeciesController.deleteSpeciesData);
+app.post("/species", SpeciesController.createSpeciesData);
+app.get("/species", SpeciesController.getAllSpeciesData);
+app.get("/species/:id", SpeciesController.getSpeciesDataById);
+app.put("/species/:id", SpeciesController.updateSpeciesData);
+app.delete("/species/:id", SpeciesController.deleteSpeciesData);
 
 //Attachments routes
-app.post("/", AttachmentsController.createAttachmentsData);
-app.get("/", AttachmentsController.getAllAttachmentsData);
-app.get("/:id", AttachmentsController.getAttachmentsDataById);
-app.put("/:id", AttachmentsController.updateAttachmentsData);
-app.delete("/:id", AttachmentsController.deleteAttachmentData);
+app.post("/attachments", AttachmentsController.createAttachmentsData);
+app.get("/attachments", AttachmentsController.getAllAttachmentsData);
+app.get("/attachments/:id", AttachmentsController.getAttachmentsDataById);
+app.put("/attachments/:id", AttachmentsController.updateAttachmentsData);
+app.delete("/attachments/:id", AttachmentsController.deleteAttachmentData);
 
 app.listen(process.env.PORT, () => {
   console.log("SERVER RUNNING");
