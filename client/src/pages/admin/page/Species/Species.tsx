9f4589aec665f890a1fa923e19b8c1124a7546c7
Roseller <enriquezroseller23@gmail.com>
2024-08-23 00:07:33 +0800

    update species updated value

--- client/src/pages/admin/page/Species/Species.tsx
+++ client/src/pages/admin/page/Species/Species.tsx
@@ -7,7 +7,7 @@ import { speciesService } from "../../../../services/species.service";
 import { SpeciesDto } from "../../../../types/species.interface";
 import GenericTable from "../../../components/GenericTable";
 import CustomMenuList from "../../../components/CustomMenuList";
-import { formatDate } from "../../../../utils/pageSize";
+import { formatReadableDate } from "../../../../utils/pageSize";
 import Toaster from "../../../components/Toaster";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
@@ -80,7 +80,7 @@ const Species = () => {
           {row.name}
         </TableCell>
         <TableCell>
-          {row?.updatedAt ? formatDate(row?.updatedAt) : ""}
+          {row?.updatedAt ? formatReadableDate(row?.updatedAt) : ""}
         </TableCell>
         <TableCell sx={{ textAlign: "right" }}>
           <CustomMenuList
@@ -97,6 +97,8 @@ const Species = () => {
       </>
     );
   };
+
+  console.log("speciesData", speciesData);
 
   return (
     <>
