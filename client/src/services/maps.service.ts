a26532d805b59f94fbaed99a72c6fc0578a34d46
rosellerenrqz <enriquezroseller23@gmail.com>
2024-08-26 15:37:30 +0800

    map service functions and reusable queries

--- client/src/services/maps.service.ts
+++ client/src/services/maps.service.ts
@@ -2,8 +2,25 @@ import { apiService } from "./api.service";
 
 const BASE_URL = "maps";
 
+//TODO: PASS IN A TYPES FOR SPECIFIC FOR MAPS DATA
 export const mapService = {
-  async getPositions() {
-    //
+  async createMaps(data: any) {
+    return apiService.post(BASE_URL, { data });
+  },
+
+  async getMaps() {
+    return apiService.get(BASE_URL);
+  },
+
+  async getOneMap(id: any) {
+    return apiService.get(`${BASE_URL}/${id}`);
+  },
+
+  async updateMaps(id: any, data: any) {
+    return apiService.put(`${BASE_URL}/${id}`, { data });
+  },
+
+  async deleteMaps(id: any) {
+    return apiService.delete(`${BASE_URL}/${id}`);
   },
 };
