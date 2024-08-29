3796b09a867dfaa741ff27f202e390ac8f4e13ad
Vincent Enriquez <vincentenriquez43@gmail.com>
2024-08-30 04:43:56 +0800

    Adding types for flipbook

--- client/src/pages/client/Flip.tsx
+++ client/src/pages/client/Flip.tsx
@@ -7,6 +7,92 @@ import { Box, Typography } from "@mui/material";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+interface FlipBookProps {
+  width: number;
+  height: number;
+  showCover?: boolean;
+  usePortrait?: boolean;
+  startPage?: number;
+  className: string;
+  style?: React.CSSProperties;
+  size?: "fixed" | "stretch";
+  minWidth?: number;
+  maxWidth?: number;
+  minHeight?: number;
+  maxHeight?: number;
+  drawShadow?: boolean;
+  flippingTime?: number;
+  startZIndex?: number;
+  autoSize?: boolean;
+  maxShadowOpacity?: number;
+  mobileScrollSupport?: boolean;
+  clickEventForward?: boolean;
+  useMouseEvents?: boolean;
+  swipeDistance?: number;
+  showPageCorners?: boolean;
+  disableFlipByClick?: boolean;
+  children: React.ReactNode;
+}
+
+const FlipBookWrapper: React.FC<FlipBookProps> = (props) => {
+  const {
+    width,
+    height,
+    showCover = false,
+    usePortrait = false,
+    startPage = 1,
+    className,
+    style,
+    size = "fixed",
+    minWidth,
+    maxWidth,
+    minHeight,
+    maxHeight,
+    drawShadow,
+    flippingTime,
+    startZIndex,
+    autoSize,
+    maxShadowOpacity,
+    mobileScrollSupport,
+    clickEventForward,
+    useMouseEvents,
+    swipeDistance,
+    showPageCorners,
+    disableFlipByClick,
+    children,
+  } = props;
+
+  return (
+    <HTMLFlipBook
+      width={width}
+      height={height}
+      showCover={showCover}
+      usePortrait={usePortrait}
+      startPage={startPage}
+      className={className}
+      style={style}
+      size={size}
+      minWidth={minWidth}
+      maxWidth={maxWidth}
+      minHeight={minHeight}
+      maxHeight={maxHeight}
+      drawShadow={drawShadow}
+      flippingTime={flippingTime}
+      startZIndex={startZIndex}
+      autoSize={autoSize}
+      maxShadowOpacity={maxShadowOpacity}
+      mobileScrollSupport={mobileScrollSupport}
+      clickEventForward={clickEventForward}
+      useMouseEvents={useMouseEvents}
+      swipeDistance={swipeDistance}
+      showPageCorners={showPageCorners}
+      disableFlipByClick={disableFlipByClick}
+    >
+      {children}
+    </HTMLFlipBook>
+  );
+};
+
 const Pages = React.forwardRef((props: any, ref: any) => {
   return (
     <div ref={ref} style={{ padding: 0, margin: 0 }}>
@@ -48,13 +134,30 @@ function Flipbook() {
         </Typography>
 
         {/* FlipBook in the middle */}
-        <HTMLFlipBook
-          width={450}
-          height={600}
-          style={{
-            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.5)",
-            borderRadius: "10px",
-          }}
+        <FlipBookWrapper
+          width={400}
+          height={570}
+          showCover={true}
+          usePortrait={true}
+          startPage={1}
+          className="flipbook"
+          style={{ overflow: "hidden" }}
+          size="fixed"
+          minWidth={200}
+          maxWidth={600}
+          minHeight={300}
+          maxHeight={800}
+          drawShadow={true}
+          flippingTime={500}
+          startZIndex={10}
+          autoSize={true}
+          maxShadowOpacity={0.5}
+          mobileScrollSupport={true}
+          clickEventForward={true}
+          useMouseEvents={true}
+          swipeDistance={50}
+          showPageCorners={true}
+          disableFlipByClick={false}
         >
           {[...Array(numPages).keys()].map((pNum) => (
             <Pages key={pNum} number={pNum + 1}>
@@ -68,7 +171,7 @@ function Flipbook() {
               </Document>
             </Pages>
           ))}
-        </HTMLFlipBook>
+        </FlipBookWrapper>
 
         {/* Page number text below the FlipBook */}
         <Typography
