2b16155f6d412b2f0f38491e25e3680069dd4267
Vincent Enriquez <vincentenriquez43@gmail.com>
2024-09-09 05:49:06 +0800

    Applying base url for service

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://98.80.9.71:3000",
+  baseURL: "http://98.80.9.71:3000/api/",
 });
 
 const cancelToken = axios.CancelToken.source();
