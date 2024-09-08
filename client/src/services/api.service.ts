fe0606200e9427b660781bfe43b507ea70fca623
Vincent Enriquez <vincentenriquez43@gmail.com>
2024-09-09 05:56:58 +0800

    ternary baseurl

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
 });
 
 const cancelToken = axios.CancelToken.source();
