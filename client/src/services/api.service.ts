d4ab2169e126db10d17888d62911e9e7f756a4e3
Vincent Enriquez <vincentenriquez43@gmail.com>
2024-09-09 05:52:05 +0800

    applying base url meta data

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://98.80.9.71:3000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const cancelToken = axios.CancelToken.source();
