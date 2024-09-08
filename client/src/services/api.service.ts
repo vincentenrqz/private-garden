affe0c234fa23c1bb092b888d61a2f9f5175c301
Vincent Enriquez <vincentenriquez43@gmail.com>
2024-09-09 06:07:37 +0800

    Revert

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const cancelToken = axios.CancelToken.source();
