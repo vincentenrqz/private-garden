3a1302d0f68ef0424c69859c19b4a963a4198797
Roseller <enriquezroseller23@gmail.com>
2024-09-17 00:29:30 +0800

    update admin drawer width in small screen

--- client/src/pages/admin/page/Map/AdminDrawer.tsx
+++ client/src/pages/admin/page/Map/AdminDrawer.tsx
@@ -13,7 +13,10 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 600,
+  width: {
+    xs: 350,
+    sm: 600,
+  },
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
