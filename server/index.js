59108c56dba66cd6f3951322a279aa4d887be5d3
Vincent Ray Enriquez <55650592+vincentenrqz@users.noreply.github.com>
2024-09-30 20:10:17 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
 //Connect to DB
 connectMongoDB();
